99e35b0c8b923c08a60b966cf76965d7735b2ce3
RyuJaeYoon <wodbs9505@gmail.com>
2023-04-26 21:19:10 +0900

    다크모드 구현

--- src/App.js
+++ src/App.js
@@ -1,44 +1,37 @@
 import React, {useState, useEffect} from "react";
 
 let AppCallCount=0;
-let SubCallCount=0;
-
-function Sub({ appNo }) {
-  SubCallCount++;
-  console.log(`SubCallCount : ${SubCallCount}`);
-
-  const [no, setNo] =useState(0);
-  const [no2, setNo2] =useState(0);
-
-  useEffect(() => { console.log("effect 1 : 단 한번 실행")},[]);
-  useEffect(() => { console.log("effect 2 : 부모(App)의 appNo가 바뀔때마다 실행"),[appNo]});
-  useEffect(() => { console.log("effect 3 : 나(sub)의 no가 바뀔 때마다 실행"),[no]});
-  useEffect(() => { console.log("effect 4 : appNo 혹은 no가 바뀔때마다 실행"),[appNo, no]});
-  useEffect(() => { console.log("effect 5 : 매번 실행")});
-  
-  return ( 
-    <>
-     <div style={{border: "10px solid blue", padding:10}}>
-      App no :{appNo}
-      <hr />
-      <button className="btn btn-outline" onClick={() => setNo(no+1)}>Sub버튼(no) : {no}</button>
-      <button className="btn btn-outline" onClick={() => setNo2(no2+1)}>Sub버튼(no) : {no2}</button>
-     </div>
-    </>
-  );
-}
 
 function App() {
   AppCallCount++;
   console.log(`AppCallCount : ${AppCallCount}`);
 
   const [no, setNo] =useState(0);
+  const [isDark, setIsDark] = useState(false);
+
+  useEffect(()=> {
+    const html = document.getElementsByTagName("html")[0];
+    
+    if (isDark) {
+      html.classList.add('dark');
+    }
+    else {
+      html.classList.remove("dark");
+    }
+  })
   return ( 
     <>
-     <div style={{border: "10px solid red", padding:10}}>
+     <div>
       <button className="btn btn-outline" onClick={() => setNo(no+1)}>App버튼 : {no}</button>
+      <button className="btn btn-outline" onClick={() => setIsDark(!isDark)}>테마토글 : {no}</button>
+      
       <hr />
-      <Sub appNo={no} />
+     
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi illum nam at eveniet. Tempora ducimus aliquid ipsa repudiandae accusamus quas magni, illum maiores? Assumenda cumque, molestias blanditiis vel placeat doloremque!
+      </div>
+
+      <h1 className= "color-pirmary">안녕,반가워</h1>
      </div>
     </>
   );
